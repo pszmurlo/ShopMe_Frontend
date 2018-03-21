da72ed81b19ccc1071b8c113f25e12b2521ca3b7
Ardneh3 <32309180+Ardneh3@users.noreply.github.com>
2018-03-21 18:50:36 +0100

    Dołączyłem odbieranie danych z serwera mockowego, jak backend dostarczy prawdziwy server to podmienię url (#19)
    feature/request_data_from_server

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import SearchInput from 'components/SearchForm/SearchInput';
 import SubmitButton from 'components/SearchForm/SubmitButton';
 
-const SearchForm = () => (
-  <form className="search__form">
-    <SearchInput />
-    <SubmitButton />
-  </form>
-);
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      services: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/offers')
+      .then(response => response.json())
+      .then(services => this.setState({ services }));
+  }
+
+  render() {
+    return (
+      <form className="search__form">
+        <SearchInput />
+        <SubmitButton />
+      </form>
+    );
+  }
+}
 
 export default SearchForm;
---
+++ src/components/SearchResults/Results.jsx
@@ -0,0 +1,50 @@
+
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+import SearchResults from 'components/SearchResults/SearchResults';
+
+class ScreensServicesResults extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      services: [
+        {
+          title: 'Koszenie trawników',
+          price: '50',
+          id: 1,
+        },
+        {
+          title: 'Wyprowadzanie psów',
+          price: '20',
+          id: 2,
+        },
+        {
+          title: 'Czyszczenie dywanów',
+          price: '100',
+          id: 3,
+        },
+        {
+          title: 'Przeprowadzki',
+          price: '200',
+          id: 4,
+        },
+        {
+          title: 'Korepetycje',
+          price: '40',
+          id: 5,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchResults services={this.props.services} />
+      </div>
+    );
+  }
+}
+
+export { ScreensServicesResults };
+export default translate()(ScreensServicesResults);
