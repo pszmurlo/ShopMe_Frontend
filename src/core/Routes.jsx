191dd7fa5703c04c03a308b8953464dd71be8ec6
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-03-28 12:41:35 +0200

    utworzenie widoku oferty

--- src/core/Routes.jsx
+++ src/core/Routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ScreensSearch from 'screens/Search/Search';
 import ScreensAddForm from 'screens/Add/Form/Form';
+import ScreensOfferProfile from 'screens/OfferProfile/OfferProfile';
 
 export default() => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ScreensSearch} />
       <Route path="/add/form" component={ScreensAddForm} />
+      <Route path="/offerprofile" component={ScreensOfferProfile} />
     </Switch>
   </BrowserRouter>
 );
---
+++ src/screens/OfferProfile/OfferProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from 'components/UI/Logo';
+
+const OfferProfile = () => (
+  <div className="offer-profile">
+    <Logo />
+  </div>
+);
+
+export default OfferProfile;
