fe4e584743b99bcf1f686696aec3db421253ef24
Ardneh3 <32309180+Ardneh3@users.noreply.github.com>
2018-04-23 23:59:45 +0200

    redirect in SuccessAdd screen (#149)

--- src/components/Add/SuccessMessage/SuccessMessage.jsx
+++ src/components/Add/SuccessMessage/SuccessMessage.jsx
@@ -1,39 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { translate } from 'react-i18next';
-import { Redirect } from 'react-router';
-
 import './SuccessMessage.css';
 
-class SuccessMessage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fireRedirect: false,
-    };
-    this.handleRedirect = this.handleRedirect.bind(this);
-  }
-
-  componentDidMount() {
-    setTimeout(this.handleRedirect, 3000);
-  }
-
-  handleRedirect() {
-    this.setState({ fireRedirect: true });
-  }
-
-  render() {
-    const { t } = this.props;
-    const { fireRedirect } = this.state;
-    if (fireRedirect) return (<Redirect to={{ pathname: '/' }} />);
-    return (
-      <section className="success-message__main-wrapper">
-        <div className="success-message__text-wrapper">
-          <h1 className="success-message__text success-message__text--green">{t('components.add.successMessage.text')}</h1>
-        </div>
-      </section>
-    );
-  }
-}
+const SuccessMessage = (props) => {
+  const { t } = props;
+  return (
+    <section className="success-message__main-wrapper">
+      <div className="success-message__text-wrapper">
+        <h1 className="success-message__text success-message__text--green">{t('components.add.successMessage.text')}</h1>
+      </div>
+    </section>
+  );
+};
 
 export { SuccessMessage };
 export default translate()(SuccessMessage);
--- src/screens/Add/SuccessAdd/SuccessAdd.jsx
+++ src/screens/Add/SuccessAdd/SuccessAdd.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { translate } from 'react-i18next';
+import { Redirect } from 'react-router';
 import SuccessMessage from 'components/Add/SuccessMessage/SuccessMessage';
 import Layout from 'core/Layout';
 
-const ScreenSuccessAdd = () => (
-  <Layout>
-    <SuccessMessage />
-  </Layout>
-);
+class ScreenSuccessAdd extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fireRedirect: false,
+    };
+    this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(this.handleRedirect, 3000);
+  }
+
+  handleRedirect() {
+    this.setState({ fireRedirect: true });
+  }
+
+  render() {
+    return (
+      <Layout>
+        { this.state.fireRedirect && <Redirect to={{ pathname: '/' }} />}
+        <SuccessMessage />
+      </Layout>
+    );
+  }
+}
 
 export { ScreenSuccessAdd };
 export default translate()(ScreenSuccessAdd);
