91a8e18b69a8d932685128422e38c750efabd588
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-05-26 20:43:24 +0200

    testing search input value

--- src/components/Search/SearchForm/SearchInput.jsx
+++ src/components/Search/SearchForm/SearchInput.jsx
@@ -29,9 +29,20 @@ class SearchInput extends React.Component {
   }
 
   validatePhrase(phrase) {
-    const cleanedSearchPhrase = phrase.replace(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}!@#$%^&*()=+\-_;:'"<>,.?/{}|`~[\]\\]/ug, '');
-    const isValidPhrase = cleanedSearchPhrase.length > 1
+    const isValid = /^[A-ZĄĘĆŁŃÓŚŹŻ0-9\s]*$/i.test(phrase);
+    let isValidPhrase;
+    const cleanedSearchPhrase = isValid ? phrase : undefined;
+    console.log(cleanedSearchPhrase);
+    // const cleanedSearchPhrase = phrase.replace(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}!@#$%^&*()=+\-_;:'"<>,.?/{}|`~[\]\\]/ug, '');
+    if (cleanedSearchPhrase) {
+      isValidPhrase = cleanedSearchPhrase.length > 1
       && Number.isNaN(Number(cleanedSearchPhrase));
+    }
+
+    if (!isValid) {
+      this.props.onSearchInputChanged(null, isValidPhrase);
+      this.setState({ errorMessage: 'Zawiera niedozwolone znaki' });
+    }
 
     this.setState({ isValidPhrase });
     if (isValidPhrase && Number.isNaN(Number(cleanedSearchPhrase)) && cleanedSearchPhrase !== '') {
