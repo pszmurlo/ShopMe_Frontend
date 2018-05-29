4925c33f4b619efabf1c5c0df11e7b148dbef2f1
Ardneh3 <32309180+Ardneh3@users.noreply.github.com>
2018-05-29 11:04:30 +0200

    test fix for header (#239)

--- src/components/App/Header/Header.test.jsx
+++ src/components/App/Header/Header.test.jsx
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { Header } from './Header';
 
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const props = {
