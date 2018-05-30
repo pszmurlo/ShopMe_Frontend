9e8b5bbfe9450fdae97724854ce926fd6712200c
Agnieszka Spiechowicz <spieszku@users.noreply.github.com>
2018-05-30 22:39:19 +0200

    Prodpfip-374 klikalny cały rezultat (#242)
    * Add search results styles
    
    * Improve styles of search results and pagination
    
    * Make whole search result clickable, add hover effect

--- src/components/Search/SearchResults/ServicesItem/ServicesItem.jsx
+++ src/components/Search/SearchResults/ServicesItem/ServicesItem.jsx
@@ -8,28 +8,27 @@ const ServicesItem = (props) => {
   const date = myDate.toLocaleDateString();
   return (
     <li className="services-item">
-      <div className="services-item__name services-item__element">
-        <Link
-          className="services-item__title"
-          to={`/offer/${props.value.id}`}
-        >
-          {props.value.title}
-        </Link>
-        <p>{props.t(`components.UI.voivodeship.list.${props.value.voivodeship}`)}, {props.value.city}</p>
-      </div>
-      <div className="services-item__element services-item__element--category">
-        <span className="services-item__span services-item__category">{props.t(`components.UI.categorySelect.categoryOptions.${props.value.category}`)}</span>
-      </div>
-      <div className="services-item__element">
-        {props.value.extendedDescription && <span className="services-item__price-prefix">{props.t('components.foundSearchResults.prefix')}</span>}
-        <span className="services-item__span services-item__price">
-          {props.value.basePrice} {props.t('components.foundSearchResults.currency')}
-        </span>
-        {props.value.extendedDescription && <p>{props.t('components.foundSearchResults.moreOptions')}</p>}
-      </div>
-      <div className="services-item__element">
-        <span className="services-item__span services-item__date">{date}</span>
-      </div>
+      <Link to={`/offer/${props.value.id}`}>
+        <div className="services-item__name services-item__element">
+          <h4 className="services-item__title">
+            {props.value.title}
+          </h4>
+          <p>{props.t(`components.UI.voivodeship.list.${props.value.voivodeship}`)}, {props.value.city}</p>
+        </div>
+        <div className="services-item__element services-item__element--category">
+          <span className="services-item__span services-item__category">{props.t(`components.UI.categorySelect.categoryOptions.${props.value.category}`)}</span>
+        </div>
+        <div className="services-item__element">
+          {props.value.extendedDescription && <span className="services-item__price-prefix">{props.t('components.foundSearchResults.prefix')}</span>}
+          <span className="services-item__span services-item__price">
+            {props.value.basePrice} {props.t('components.foundSearchResults.currency')}
+          </span>
+          {props.value.extendedDescription && <p>{props.t('components.foundSearchResults.moreOptions')}</p>}
+        </div>
+        <div className="services-item__element">
+          <span className="services-item__span services-item__date">{date}</span>
+        </div>
+      </Link>
     </li>
   );
 };
